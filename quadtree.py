c38bdf7cd328c019a5c2f6899d3dc49e95f62cdc
Kristofer Munsterhjelm <kristofer@munsterhjelm.no>
2024-12-01 23:12:30 +0100

    Write a test program for the quadtree, and do more quadtree work.

--- quadtree.py
+++ quadtree.py
@@ -1,4 +1,5 @@
-# Implement a relatively simple quadtree for the redistricter.
+# Implement a relatively simple quadtree, with relatively
+# less simple additional behavior, for the redistricter.
 
 # Also needs an iterator "leaves".
 
@@ -55,6 +56,8 @@ The inheritance rules are:
 	If all our children are out of bounds, so are we.
 '''
 
+import numpy as np
+
 RQ_STALE = 0
 RQ_UNDECIDED = 1
 RQ_DECIDED = 2
@@ -62,43 +65,108 @@ RQ_OUT_OF_BOUNDS = 3
 RQ_PARTLY_OUT_OF_BOUNDS = 4
 
 class RQuadtree:
-	def __init__(self, upper_left, lower_right):
+	# Initialize as a root node by default
+	def __init__(self, upper_left=None, lower_right=None):
+		if upper_left is None and lower_right is None:
+			upper_left = (0, 0)
+			lower_right = (1., 1.)
+
 		# In (x,y) number coordinates
-		self.upper_left_idx = upper_left
-		self.lower_right_idx = lower_right
+		self.upper_left_idx = np.array(upper_left)
+		self.lower_right_idx = np.array(lower_right)
 
 		# These are in the order upper left, upper right,
 		# lower left, lower right.
 		self.children = [None] * 4
+		self.leaf = True
 
 		self.state = RQ_STALE
-
-	# Initialize as a root node.
-	def __init__(self):
-		self.__init__( (0, 0), (2**64, 2**64) )
 
 	def _mid(self, a, b):
 		if a > b:
 			return b + (a-b)/2
 		return a + (b-a)/2
 
+	def get_undecided_points(self):
+		if self.leaf:
+			if self.state == RQ_UNDECIDED:
+				return set(self.upper_left_idx, self.lower_right_idx)
+			return set()
+
+		if self.state != RQ_UNDECIDED:
+			return set()
+		
+		joined_points = set()
+
+		for child in self.children:
+			joined_points = joined_points.union(
+				child.get_undecided_points())
+
+		return joined_points
+
 	def split(self):
-		# TODO: Check if we've already split; if so, raise
-		# an exception.
+		if not self.leaf:
+			raise Exception("Quadtree: trying to split a non-leaf node!")
+
+		self.leaf = False
 
 		# The x and y coordinates are in left-middle-right or top-middle-bottom
 		# order.
 
-		x = [upper_left[0],
-			 _mid(upper_left[0], lower_left[0]),
-			 lower_left[0]]
+		x = [self.upper_left_idx[0],
+			 self._mid(self.upper_left_idx[0], self.lower_right_idx[0]),
+			 self.lower_right_idx[0]]
 
-		y = [upper_left[1],
-			 _mid(upper_left[1], lower_left[1])
-			 lower_left[1]]
+		y = [self.upper_left_idx[1],
+			 self._mid(self.upper_left_idx[1], self.lower_right_idx[1]),
+			 self.lower_right_idx[1]]
 
-		self.child[0] = RQuadtree( (x[0], y[0]), (x[1], y[1]) )
-		self.child[1] = RQuadtree( (x[1], y[0]), (x[2], y[1]) )
+		self.children[0] = RQuadtree( (x[0], y[0]), (x[1], y[1]) )
+		self.children[1] = RQuadtree( (x[1], y[0]), (x[2], y[1]) )
 
-		self.child[2] = RQuadtree( (x[0], y[1]), (x[1], y[2]) )
-		self.child[2] = RQuadtree( (x[1], y[1]), (x[2], y[2]) ) 
+		self.children[2] = RQuadtree( (x[0], y[1]), (x[1], y[2]) )
+		self.children[3] = RQuadtree( (x[1], y[1]), (x[2], y[2]) ) 
+
+	# Change a stale node into some other kind of node based on the
+	# current tree structure, before we determine the values of the
+	# nodes.
+
+	def update_state_before_resolving(self, bounding_square_ul,
+		bounding_square_lr, nat_upper_left, nat_lower_right):
+
+		# Assume that 1 is the whole range from bounding_square_ul
+		# to bounding_square_lr.
+
+		span = (bounding_square_lr - bounding_square_ul)
+
+		# In what direction (if any) do we extend past the natural
+		# bounds in the upper (northwest) direction?
+		exceeds_upper = np.logical_or(
+			bounding_square_ul + span * self.upper_left_idx < nat_upper_left,
+			bounding_square_ul + span * self.upper_left_idx > nat_lower_right)
+
+		exceeds_lower = np.logical_or(
+			bounding_square_ul + span * self.lower_right_idx < nat_upper_left,
+			bounding_square_ul + span * self.lower_right_idx > nat_lower_right)
+
+		if np.all(exceeds_upper) and np.all(exceeds_lower):
+			self.state = RQ_OUT_OF_BOUNDS
+			return
+
+		if np.any(exceeds_upper) or np.any(exceeds_lower):
+			self.state = RQ_PARTLY_OUT_OF_BOUNDS
+
+	# Check if we're partially out of bounds, and if so, split.
+	# If we're a leaf node, completely out of bounds and don't know it,
+	# set our state to out_of_bounds as well.
+	def split_on_bounds(self, bounding_square_ul, bounding_square_lr):
+		if not self.leaf and self.state == RQ_PARTLY_OUT_OF_BOUNDS:
+			for child in self.children:
+				child.split_on_bounds(bounding_square_ul,
+					bounding_square_lr)
+
+		# If we're not partly out of bounds or stale, there's no
+		# out-of-bounds to split.
+		if self.state == RQ_DECIDED or self.state == RQ_UNDECIDED or \
+			self.state == RQ_OUT_OF_BOUNDS:
+			return
---
+++ tree_tester.py
@@ -0,0 +1,37 @@
+# Mockup concept for the quadtree.
+
+# We create a rectangle of 2:1 aspect ratio with a diagonal going through it.
+# We then define the quadtree to cover the whole rectangle; as a square, this
+# gives it an out-of-bounds region.
+
+# (I could also just do it anamorphically but whatever)
+
+from quadtree import RQuadtree
+import numpy as np
+
+nat_upper_left = np.array((0, 0))
+nat_lower_right = np.array((1, 0.5))
+
+bounding_square_ul = nat_upper_left
+bounding_square_lr = np.array([np.max(nat_lower_right), np.max(nat_lower_right)])
+
+# The point must be in natural units, i.e. the units above, not
+# the quadtree's 0..2**64 scale.
+
+def get_value(point):
+
+	x, y = point
+
+	if x < 0 or x > 1:
+		raise KeyError("get_value: x is out of bounds")
+
+	if y < 0 or y > 0.5:
+		raise KeyError("get_value: y is out of bounds")
+
+	# The diagonal is defined by y = x/2.
+	if x >= y * 2:
+		return 0		# first color
+
+	return 1			# second color
+
+root = RQuadtree()
