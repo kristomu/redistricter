Hack-fix lat/long rotation problem in lp_quadtree; extract geometry functs.
Also fix a bug in tree_tester that would result in weird results if
the upper left of the region is not at (0,0), and add a "write to PNG"
function to lp_quadtree.

lp_district.py
@@ -29,68 +29,11 @@ import subprocess
 from PIL import Image
 
 from problems import *
+from spheregeom import *
 from quant_tools import grid_dimensions
 
-# https://stackoverflow.com/a/20360045
-# Input coordinates are in radians.
-def LLHtoECEF_rad(r_lat, r_lon, alt):
-	# see http://www.mathworks.de/help/toolbox/aeroblks/llatoecefposition.html
-
-	rad = np.float64(6378137.0)        # Radius of the Earth (in meters)
-	f = np.float64(1.0/298.257223563)  # Flattening factor WGS84 Model
-	cosLat = np.cos(r_lat)
-	sinLat = np.sin(r_lat)
-	FF     = (1.0-f)**2
-	C      = 1/np.sqrt(cosLat**2 + FF * sinLat**2)
-	S      = C * FF
-
-	x = (rad * C + alt)*cosLat * np.cos(r_lon)
-	y = (rad * C + alt)*cosLat * np.sin(r_lon)
-	z = (rad * S + alt)*sinLat
-
-	return (x, y, z)
-
-def LLHtoECEF_latlon(lat, lon, alt):
-	return LLHtoECEF_rad(np.radians(lat), np.radians(lon), alt)
-
-# https://stackoverflow.com/a/29546836
-def haversine_np(lat1, lon1, lat2, lon2):
-    """
-    Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    
-    All args must be of equal length.    
-    
-    """
-    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
-    
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    
-    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
-    
-    c = 2 * np.arcsin(np.sqrt(a))
-    km = 6378.137 * c
-    return km
-
-# Input is center as a (lat, long) array, as well as a list of
-# other points as an array of (lat, long) arrays. It returns an
-# array of distances from the given center to each of the other points.
-def haversine_center(center, other_points):
-	duped_center = np.repeat([center], len(other_points), axis=0)
-
-	return haversine_np(duped_center[:,0], duped_center[:,1],
-		other_points[:,0], other_points[:,1])
-
-def haversine_centers(centers_latlon, other_points):
-	each_center_iter = map(
-		lambda center: haversine_center(center, other_points),
-		centers_latlon)
-
-	return np.array(list(each_center_iter))
-
 # Draws an entry from 0...num_entries proportional to weight.
-def draw(num_entries, weight):
+def random_draw(num_entries, weight):
 	norm_divisor = np.sum(weight)
 
 	return np.random.choice(range(num_entries), p = weight/norm_divisor)
@@ -108,7 +51,7 @@ def kmpp(population_counts, distance_generator, num_clusters):
 	minimal_distances = np.array([np.inf] * num_points)
 
 	# Pick initial point
-	draws = [draw(num_points, population_counts)]
+	draws = [random_draw(num_points, population_counts)]
 
 	for i in range(1, num_clusters):
 		# Update the minimal distances array to take the last added
@@ -124,7 +67,7 @@ def kmpp(population_counts, distance_generator, num_clusters):
 
 		# Draw another entry.
 
-		new_entry = draw(num_points, kmpp_weight)
+		new_entry = random_draw(num_points, kmpp_weight)
 
 		# Append it to the list of chosen points.
 		draws.append(new_entry)
@@ -322,8 +265,6 @@ def redistrict(desired_num_districts, district_indices, verbose=False,
 		grid_populations[cur_block["center_idx"]] += cur_block["population"]
 
 	# 5. Create pairwise distances between centers and points.
-	# We need squared distances because the objective is to minimize the sum of
-	# squared distances - we want a k-means generalization, not a k-medians.
 	district_point_dist = haversine_centers(
 		district_latlon, grid_latlon)
 

lp_quadtree.py
@@ -1,5 +1,12 @@
 # Trying to reimplement the LP stuff to use quadtrees.
 
+# detected bugs: I must be multiplying by the span somewhere that I shouldn't,
+# because it's creating a box that has got the right aspect ratio, but it's
+# canceling out the aspect ratio calculation somewhere inside, so everything
+# gets stretched. In this case, I have a 2:1 aspect ratio (rotated due to lat/long
+# issues), and it stretches the vertical coordinates in a way that would make sense
+# if it were rendering something with an 2:1 aspect anamorphically on an 1:1 output.
+
 from parse_dbf import get_state_names, get_census_block_data
 from scipy.spatial import cKDTree, Delaunay
 import numpy as np
@@ -19,9 +26,31 @@ from quant_tools import grid_dimensions
 
 from quadtree import RQuadtree
 
-# Normalized coordinates to natural ones
+# Normalized coordinates to natural ones.
 def to_natural(normalized, minimum_point, maximum_point):
-	return minimum_point + np.array(normalized) * (maximum_point-minimum_point)
+	latlong = minimum_point + np.array(normalized) * (bounding_square_lr - minimum_point)
+	return latlong
+
+def write_image(filename, resolution, quadtree):
+
+	# Transpose because the quadtree has latitudes assigned to its
+	# x coordinate and longitudes to its y coordinate.
+	image_space_claimants = quadtree.linearize(resolution).T.astype(np.int8)
+	claimed_num_districts = (int)(np.max(image_space_claimants)+1)
+
+	print(image_space_claimants, claimed_num_districts)
+
+	# Create some random colors.
+	colors = np.random.randint(256, size = (claimed_num_districts, 3),
+		dtype=np.uint8)
+
+	# Color mixed claims grey.
+	colors = np.vstack([colors, [127, 127, 127]])
+	image_space_claimants[image_space_claimants==-1] = claimed_num_districts
+
+	# And save!
+	image = Image.fromarray(colors[image_space_claimants].astype(np.uint8))
+	image.save(filename, "PNG")
 
 # 1. Get coordinates and populations
 #	  Use Colorado for now.
@@ -83,7 +112,7 @@ for index in district_indices:
 district_coords = np.array(district_coords)
 district_latlon = np.array(district_latlon)
 
-for i in range(4):
+for i in range(8):
 	print("Iteration", i)
 	# QT
 	# TODO: Fix quadtree bug where incredibly narrow rectangles

tree_tester.py
@@ -13,7 +13,9 @@ nat_upper_left = np.array((0, 0))
 nat_lower_right = np.array((1, 0.5))
 
 bounding_square_ul = nat_upper_left
-bounding_square_lr = np.array([np.max(nat_lower_right), np.max(nat_lower_right)])
+bounding_square_lr = nat_upper_left + np.array([
+	np.max(nat_lower_right-nat_upper_left),
+	np.max(nat_lower_right-nat_upper_left)])
 
 # The quadtree uses "normalized" units, where the root cell ranges from 0 to 1 on
 # both axes, with (0,0) anchored at the upper left of the natural bounding